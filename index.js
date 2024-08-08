782c1f3fc61420073f4863591cd33eabfa7a7aeb
Abdulrahim341 <abdulrahimelaidy94@gmail.com>
2024-08-09 00:56:57 +0300

    stripe added

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ import express from 'express'
 import bootstrab from './src/bootstrab.js'
 import cors from 'cors'
 import 'dotenv/config'
+import asyncHandler from './src/middleware/asyncHandler.js'
+import Stripe from 'stripe'
+const stripe = new Stripe('sk_test_51PlFxXLs68W79KXq5qwD227XeRApbWSlkK1lE2A5WclJdYOwY2U5Dd8SV9ilvS4cry2ptLWmBvEw2ir3NhX60dg000MSdQgLT0');
 
 
 const app = express()
 const port = process.env.PORT ||3000
+
+app.post('api/vi/webhook',express.raw({type:'application/json'}),asyncHandler((req,res)=>{
+    const sig=req.headers['stripe-signature'].toString()
+    let event = stripe.webhooks.constructEvent(req.body,sig,"whsec_0qaIk9bLL8VlAKvFiPM6VijsBc111p6I")
+    let chechout
+    if(event.type=="checkout.session.completed"){
+        chechout=event.data.object
+    }
+    res.json({message:"success",chechout})
+}))
 app.use(cors())
 // const port=3000
 bootstrab(app,express)
